a7b073e8c8262ff78fb9d13d38bf25eed9a307d3
robert-min <robertmin522@gmail.com>
2023-12-08 12:55:53 +0900

    FIX db_manager MYSQL_CONNECTION path to move __init__.py

--- src/libs/__init__.py
+++ src/libs/__init__.py
@@ -1,9 +1,11 @@
 import os
 import json
 
+# TODO : conf 파일 별도 폴더로 관리
 libs_path: str = os.path.abspath(os.path.join(__file__, os.path.pardir))
 conf_file: str = os.path.abspath(os.path.join(libs_path, "conf.json"))
 with open(conf_file, "rt") as f:
     conf = json.load(f)
-    
+
 ENCRYPTION_KEY = conf["encrypt_dek"]
+MYSQL_CONNECTION = conf["mysql_connection"]
--- src/libs/db_manager.py
+++ src/libs/db_manager.py
@@ -1,17 +1,7 @@
-
-import os
-import json
-
 import typing as t
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, DeclarativeBase
-
-# TODO : conf 파일 별도 폴더로 관리
-libs_path: str = os.path.abspath(os.path.join(__file__, os.path.pardir))
-conf_file: str = os.path.abspath(os.path.join(libs_path, "conf.json"))
-with open(conf_file, "rt") as f:
-    conf = json.load(f)
-MYSQL_CONNECTION = conf["mysql_connection"]
+from . import MYSQL_CONNECTION
 
 class Base(DeclarativeBase):
     pass
